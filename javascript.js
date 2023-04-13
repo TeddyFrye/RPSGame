99c789d76013d67a7239a6296ffacd6fb671cf0e
TeddyFrye <tfrye1227@gmail.com>
2023-04-13 12:18:16 -0500

    Changes from console.log to returning result

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
-function playRound(playerChoice, compChoice){
+function playRound(playerChoice, compChoice) {
     const player = playerChoice.toLowerCase();
     const computer = compChoice.toLowerCase();
-    if (player === "rock"&& computer === "scissors"){
-        console.log("Rock beats Scissors, You Win the Round");}
-    else if (player === "scissors" && computer === "paper"){
-        console.log("Scissors beats Paper, You Win the Round");}
-    else if (player === "paper" && computer === "rock"){
-        console.log("Surprisingly, Paper does beat Rock. You Win the Round")}
-    else if ((player != "rock") && (player != "scissors") && (player != "paper")){
-        console.log("Improper input");}
-    else {console.log("You lose! Computer wins.")};
-}
-
-
-
-console.log(playRound("Paper","SCissors"));
+  
+    if (player === "rock" && computer === "scissors") {
+      return "Rock beats Scissors, You Win the Round";
+    } else if (player === "scissors" && computer === "paper") {
+      return "Scissors beats Paper, You Win the Round";
+    } else if (player === "paper" && computer === "rock") {
+      return "Surprisingly, Paper does beat Rock. You Win the Round";
+    } else if (player !== "rock" && player !== "scissors" && player !== "paper") {
+      return "Improper input";
+    } else {
+      return "You lose! Computer wins.";
+    }
+  }
+  
+  console.log(playRound("rock", "paper")); 
+  
